1e9966e783e10dadf7d45e05a3e2664076df3d03
ridhotitipku <107243155+ridhotitipku@users.noreply.github.com>
2022-10-12 22:27:36 +0700

    Initial Commit

---
+++ YoutubeClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YoutubeClone
+//
+//  Created by ridho mujizat on 12/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YoutubeClone/YoutubeCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeCloneApp.swift
+//  YoutubeClone
+//
+//  Created by ridho mujizat on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
